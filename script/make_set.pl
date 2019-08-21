793c2284c3df11779cab7ec1d2898a6592a40529
Rutger Vos <rutgeraldo@gmail.com>
2019-08-21 22:05:30 +0200

    regenerated

--- script/make_set.pl
+++ script/make_set.pl
@@ -9,12 +9,15 @@ use Bio::Phylo::Util::Logger ':simple';
 # process command line arguments
 my $datafile;
 my $treefile;
-my $column;
+my $column;  # column in $datafile that contains the states of interest
+my @states;  # state(s) that need to be present for a shift to be of interest
+my $mindata = 100; # number of occurrences (on either side) for shifts to be interesting
 my $verbosity = WARN;
 GetOptions(
 	'datafile=s' => \$datafile,
 	'treefile=s' => \$treefile,
 	'column=s'   => \$column,
+	'states=s'   => \@states,
 	'verbose+'   => \$verbosity,
 );
 	
@@ -22,77 +25,77 @@ GetOptions(
 Bio::Phylo::Util::Logger->VERBOSE( '-level' => $verbosity, '-class' => 'main' );
 my $data = My::Taxon->connect( $datafile )->resultset( 'Taxon' );
 my $tree = Bio::Phylo::Forest::DBTree->connect( $treefile);
+my $node = $tree->resultset('Node');
 my $root = $tree->get_root;
-my $n = $data->search( { $column => \'IS NOT NULL' } )->count;
+my $taxa = $data->search( { $column => \'IS NOT NULL' } );
+my $n = $taxa->count;
 
-# we have to do two passes, first postorder to label the nodes:
-INFO "Attaching states to $n nodes by postorder traversal";
+# we have to do two passes, first to label the nodes:
+INFO "Attaching states to $n nodes";
 my $i = 1;
-$root->visit_depth_first(
-	'-post' => sub {
-		my $node = shift;
-		
-		# here we just label the tips
-		if ( $node->is_terminal ) {
-			my $id = $node->get_id;
-			my $rs = $data->search( { 'allmb_id' => $id } );
-			while( my $r = $rs->next ) {
-				if ( my $state = $r->$column ) {
-					$node->set_generic( $column => { $state => [$r] } );
-					DEBUG "Attached $column => $state to tip $i/$n";
-					$i++;
-				}
-			}
+my %anc;
+TAXON: while( my $taxon = $taxa->next ) {
+
+	# attach taxon to tip
+	my $state = $taxon->$column;
+	my $tip   = $node->find($taxon->allmb_id);
+	my $tipid = $tip->id;
+	$anc{$tipid} = $tip;
+	$tip->set_generic( $column => { $state => $taxon } );
+	DEBUG "Attached $column => $state to tip $i/$n";
+	$i++;
+	
+	# carry over to ancestors
+	my $ancrs = $tip->get_ancestors_rs;
+	while( my $anc = $ancrs->next ) {
+		my $cache    = $anc->get_generic($column) || {};
+		my $exemplar = $cache->{$state} || $taxon;	
+		my $ancid    = $anc->id;
+		$anc{$ancid} = $anc;			
+		if ( ( $taxon->num_occurrences || 0 ) >= ( $exemplar->num_occurrences || 0 ) ) {
+			$cache->{$state} = $taxon;
+			$anc->set_generic( $column => $cache );	
 		}
-		
-		# here we merge the tip states
 		else {
-			my %state;
-			for my $child ( @{ $node->get_children } ) {
-				if ( my $cache = $child->get_generic($column) ) {
-					for my $key ( keys %$cache ) {
-						$state{$key} = [] if not $state{$key};
-						push @{ $state{$key} }, @{ $cache->{$key} };
-					}
-				}
-			}
-			$node->set_generic( $column => \%state );
-		}
+			next TAXON;
+		}		
 	}
-);
+}
 
 # second we do a pre-order traversal to fetch the earliest monotypic node
 INFO "Going to find synapomorphy exemplars by pre-order traversal";
 my @header = ( qw(allmb_name gbif_species_key num_occurrences), $column );
 print join("\t",@header), "\n";
-$root->visit_depth_first(
-	'-pre' => sub {
-		my $node = shift;
-		
-		# check if node is annotated: might not be if none of the subtended tips had data
-		if ( my $cache = $node->get_generic($column) ) {
-		
-			# if there is only one key the subtended clade is monotypic
-			if ( scalar(keys(%$cache)) == 1 ) {
-				DEBUG "Found monotypic internal node";
+my %seen;
+
+for my $node ( sort { $a->left <=> $b->left } values %anc ) {
+	my $nid = $node->id;
+	next if $seen{$nid};
+
+	# if there is only one key the subtended clade is monotypic
+	my %cache = %{ $node->get_generic($column) };
+	if ( scalar(keys(%cache)) == 1 and scalar(grep {defined} map {$cache{$_}} @states) ) {
+		DEBUG "Found monotypic node of interest";
 				
-				# sort by num occurrences (desc) using Schwartzian transform
-				my ($tips) = values %$cache;
-				my ($most_occurrences) = map { $_->[1] } 
-				                        sort { $b->[0] <=> $a->[0] }
-				                         map { [ $_->num_occurrences || 0, $_ ] } @$tips;
-				my @values = map { $most_occurrences->$_ } @header;
-				print join("\t",@values), "\n";
-				
-				# unset all the descendents
-				my $desc_rs = $node->get_descendants_rs;
-				while( my $desc = $desc_rs->next ) {
-					$desc->set_generic( $column => undef );
-				}
-			}
+		my $parent = $node->get_parent;			
+		my ($n) = values %cache;
+		my @values = map { $n->$_ } @header;
+		print join("\t",@values), "\n";		
+		
+		INNER: for my $s ( values %{ $parent->get_generic($column) } ) {
+			next INNER if $s->id == $n->id;
+			my @values = map { $s->$_ } @header;
+			print join( "\t", @values ), "\n";		
+		}
+		
+		my ( $l, $r ) = ( $parent->left, $parent->right );
+		for my $desc ( grep { $_->left > $l && $_->right < $r } values %anc ) {
+			my $did = $desc->id;
+			$seen{$did}++;
+			DEBUG $did;
 		}
 	}
-);
+}
 	
 
 
