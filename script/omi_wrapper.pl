49b1cc0a66c606f46491cc8de07add13b8089e59
Rutger Vos <rutgeraldo@gmail.com>
2019-08-26 12:06:26 +0200

    doing this inefficiently one taxon at a time. upshot is that we can identify exactly which ones are failing.

--- script/omi_wrapper.pl
+++ script/omi_wrapper.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-for ( my $i = 1; $i <= 1600; $i += 100 ) {
-    my $j = $i + 99;
-    system("./OMI.R -f $i -l $j -o ../data/niche_traits.csv -c ../config.yml");
+for my $i ( 309 .. 1585 ) {
+    system("./OMI.R -f $i -l $i -o ../data/niche_traits$i.csv -c ../config.yml");
 }
